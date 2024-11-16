e37db62ceceb632dd09136dc4a8075aa5ec44a5e
Gabriel Baiduc <baiducgabriel@gmail.com>
2024-11-16 21:43:48 +0200

    autofilled and broken, circular imports or an obscure infinite recursion

--- src/python/litenet/tensor.py
+++ src/python/litenet/tensor.py
@@ -1,17 +1,12 @@
-# Import the compiled C extension
-import litenet
+from ._litenet_core import Tensor as _CTensor
 
 class Tensor:
     def __init__(self, shape):
-        # Create a Tensor object using the C extension
-        self._tensor = litenet.Tensor(shape)
+        self._tensor = _CTensor(shape)
 
     def __repr__(self):
         return f"Tensor(shape={self.shape})"
 
     @property
     def shape(self):
-        # Access the shape of the tensor
         return self._tensor.shape
-
-    # Add more methods as needed to interface with the C extension
