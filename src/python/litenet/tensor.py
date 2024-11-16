a57fe85627342ba3d6cd426343fe0cddb5e7eddc
Gabriel Baiduc <baiducgabriel@gmail.com>
2024-11-16 21:45:23 +0200

    temp disabled until python/c binding reviewed

--- src/python/litenet/tensor.py
+++ src/python/litenet/tensor.py
@@ -1,12 +1,12 @@
-from ._litenet_core import Tensor as _CTensor
+# # from ._litenet_core import Tensor as _CTensor
 
-class Tensor:
-    def __init__(self, shape):
-        self._tensor = _CTensor(shape)
+# class Tensor:
+#     def __init__(self, shape):
+#         # self._tensor = _CTensor(shape)
 
-    def __repr__(self):
-        return f"Tensor(shape={self.shape})"
+#     def __repr__(self):
+#         return f"Tensor(shape={self.shape})"
 
-    @property
-    def shape(self):
-        return self._tensor.shape
+#     @property
+#     def shape(self):
+#         return self._tensor.shape
