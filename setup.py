bf39a757b299fa341dc56cd40e45addf9d65cc70
Gabriel Baiduc <baiducgabriel@gmail.com>
2024-11-17 17:42:45 +0200

    we're telling setup tools about our compiled shared library file (a-la CMake), our 'litenet' python package didn't know it exists

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ setup(
     version="0.1.0",
     package_dir={"": "src/python"},
     packages=find_packages(where="src/python"),
+    package_data={
+        'litenet': ['_litenet_core.*']
+    },
     description="A lightweight machine learning library",
     install_requires=[
         "numpy>=1.20.0",
